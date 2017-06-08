253f98e99221695c72903841b49aae6f743b35ec
MSI\Raimo van Berkel <99040439@mydavinci.nl>
2017-06-08 11:11:11 +0200

    no message

--- game.js
+++ game.js
@@ -1,27 +1,97 @@
 function naarvolgend()
 {
+	if (level==6) {
+		level=13;
+	} else{
+	level=level+1;
+	}
+
+	if (level==11) {
+		level=6;
+	}
+
+	if (level==12) {
+		level=14;
+	}
 console.log ("van "+level);
-level=level+1;	
 console.log ("naar volgend "+level);
 actueellevel(level);
 }
 
 function naarvorig()
 {
+	if (level==12) {
+		level=4;
+	}
+	else{
+		if (level==9) {
+			level=4;
+		}
+	}
+if (level==13) {
+	level=7;
+
+	if (level==12) {
+		level=6;
+	}
+}
+
 console.log ("van  "+level);
 level=level-1;	
 console.log ("naar vorig "+level);
 actueellevel(level);
 }
 
+function naarextra1 (){
+if (level==3) {
+level=9;
+}
+else {
+	if (level==6) {
+		level=7;
+	}
+}
+
+console.log ("naar extra "+level);
+actueellevel(level);
+}
+
+function naarextra2 (){
+	if (level==3) {
+		level=12; 
+	} else 
+	{
+		if (level==6)
+			{level=11
+			}
+
+
+	}
+
+console.log ("naar extra "+level);
+actueellevel(level);
+}
+
+
 function actueellevel()
 {
 console.log("actueellevel " + level);
 
-if (level==null) {level=0;}	
-if (level==-1) {level=0;}	
-if (level==8) {document.getElementById("volgend").hidden=true;} else {document.getElementById("volgend").hidden=false;}
-if (level==0) {document.getElementById("vorig").hidden=true;} else 	{document.getElementById("vorig").hidden=false;}
+	if (level==null) {level=0;}	
+	if (level==-1) {level=0;}	
+	if (level==8) {
+	document.getElementById("volgend").style.visibility="hidden" ;
+	document.getElementById("vorig").style.visibility="hidden";
+	} else { 
+		if (level==0 || level==1) {document.getElementById("vorig").style.visibility="hidden";} else 	{document.getElementById("vorig").style.visibility="visible";}
+		if (level==3 || level==6) {
+			document.getElementById("extra1").style.visibility="visible";
+			document.getElementById("extra2").style.visibility="visible";
+			} else {
+			document.getElementById("extra1").style.visibility="hidden";
+			document.getElementById("extra2").style.visibility="hidden";
+			}
+		}
 
 kamer = [
 		"Het Kasteel",
@@ -49,15 +119,16 @@ document.getElementById("kasteel").src = "kasteel"+level+".gif";
 console.log (document.getElementById("kasteel").src) ;
 }
 
-
+/*verder*/
 function meldn(){
-var tekstmelden = ["Klik om te starten","Naar volgende kamer","Naar 3","Naar 4","Naar 5","Naar 6", "Naar 7", "Naar 8", "Oeps gaat niet verder" ];
+var tekstmelden = ["Klik om te starten","Deur 2","Deur 3","Deur 4","Deur 5","Deur 6", "Deur 7", "Deur 8","Deur 9","Deur 10","Deur 6", "Deur dicht", "Deur val", "Deur gevangenis", "dood" ];
 var  loc=level
 return tekstmelden[loc];
 }
 
+/*terug*/
 function meldp(){
-var tekstmelden = ["Oeps hier kan je niet terug","Naar begin","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Terug Naar 7" ];
+var tekstmelden = ["Oeps hier kan je niet terug","Naar begin niet mogelijk","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Einde","Naar 3","Naar 9","Naar 6","Naar 3","Naar 6","Level 14" ];
 var  loc=level
 return tekstmelden[loc];
 }
@@ -65,12 +136,16 @@ return tekstmelden[loc];
 
 
 function uilinv(){
-document.getElementById("uiltje").hidden=true;
-document.getElementById("invuil").hidden=false;
+document.getElementById("uiltje").style.visibility="hidden";
+document.getElementById("invuil").style.visibility="visible";
 }
 
 
 function uilmov(){
-document.getElementById("uiltje").style.display="block";
+document.getElementById("uiltje").style.visibility="visible";
 }
 
+function uildbl(){
+document.getElementById("uiltje").style.visibility="visible";
+document.getElementById("invuil").style.visibility="hidden";
+}
--- test/game.js
+++ test/game.js
@@ -1,7 +1,15 @@
 function naarvolgend()
 {
+	if (level==6) {
+		level=13;
+	} else{
+	level=level+1;
+	}
+
+	if (level==11) {
+		level=6;
+	}
 console.log ("van "+level);
-level=level+1;	
 console.log ("naar volgend "+level);
 actueellevel(level);
 }
@@ -14,14 +22,53 @@ console.log ("naar vorig "+level);
 actueellevel(level);
 }
 
+function naarextra1 (){
+if (level==3) {
+level=9;
+}
+else {
+	if (level==6) {
+		level=7;
+	}
+}
+
+console.log ("naar extra "+level);
+actueellevel(level);
+}
+
+function naarextra2 (){
+	if (level==3) {
+		level=12; 
+	} else 
+	{
+		if (level==6)
+			{level=11
+			}
+	}
+console.log ("naar extra "+level);
+actueellevel(level);
+}
+
+
 function actueellevel()
 {
 console.log("actueellevel " + level);
 
-if (level==null) {level=0;}	
-if (level==-1) {level=0;}	
-if (level==8) {document.getElementById("volgend").hidden=true;} else {document.getElementById("volgend").hidden=false;}
-if (level==0) {document.getElementById("vorig").hidden=true;} else 	{document.getElementById("vorig").hidden=false;}
+	if (level==null) {level=0;}	
+	if (level==-1) {level=0;}	
+	if (level==8) {
+	document.getElementById("volgend").style.visibility="hidden" ;
+	document.getElementById("vorig").style.visibility="hidden";
+	} else { 
+		if (level==0 || level==1) {document.getElementById("vorig").style.visibility="hidden";} else 	{document.getElementById("vorig").style.visibility="visible";}
+		if (level==3 || level==6) {
+			document.getElementById("extra1").style.visibility="visible";
+			document.getElementById("extra2").style.visibility="visible";
+			} else {
+			document.getElementById("extra1").style.visibility="hidden";
+			document.getElementById("extra2").style.visibility="hidden";
+			}
+		}
 
 kamer = [
 		"Het Kasteel",
@@ -51,13 +98,13 @@ console.log (document.getElementById("kasteel").src) ;
 
 
 function meldn(){
-var tekstmelden = ["Klik om te starten","Naar volgende kamer","Naar 3","Naar 4","Naar 5","Naar 6", "Naar 7", "Naar 8", "Oeps gaat niet verder" ];
+var tekstmelden = ["Klik om te starten","Deur 2","Deur 3","Deur 4","Deur 5","Deur 6", "Deur 13", "Deur 8","Einde","Deur 10","Deur 6", "Deur dicht", "Deur val", "Deur gevangenis", "Oeps gaat niet verder" ];
 var  loc=level
 return tekstmelden[loc];
 }
 
 function meldp(){
-var tekstmelden = ["Oeps hier kan je niet terug","Naar begin","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Terug Naar 7" ];
+var tekstmelden = ["Oeps hier kan je niet terug","Naar begin niet mogelijk","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Einde","Naar 3","Naar 9","Naar 6","Naar 3","Naar 6","Level 14?" ];
 var  loc=level
 return tekstmelden[loc];
 }
@@ -65,12 +112,12 @@ return tekstmelden[loc];
 
 
 function uilinv(){
-document.getElementById("uiltje").hidden=true;
-document.getElementById("invuil").hidden=false;
+document.getElementById("uiltje").style.visibility="hidden";
+document.getElementById("invuil").style.visibility="visible";
+
 }
 
 
 function uilmov(){
-document.getElementById("uiltje").style.display="block";
+document.getElementById("uiltje").style.visibility="visible";
 }
-
