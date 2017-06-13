db4c6bda2acd41df513e370328f9dd24ebd28ab8
MSI\Raimo van Berkel <99040439@mydavinci.nl>
2017-06-13 22:47:10 +0200

    no message

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-	inhrugz=[true,false,true,false,false,false,false,false,false,false,false,true,false,false,false,false,false];
+	inhrugz=[true,false,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true];
 
 
 
@@ -23,23 +23,16 @@ if (inhrugz[level]==true)
 	}
 
 	if (level==12) {
-		level=14;
+	document.getElementById("volgend").style.visibility="hidden";	;
 	}
 
 
 } else
 {
-alert("Je kunt nog niet verder, misschien mis je nog iets!   :)");
+alert("Je kunt nog niet verder, misschien mis je nog iets!   :')");
 }
 
-//var volgendknop = document.getElementById('volgend');
-//if (level==0) {
-//	volgendknop.style.visibility=== 'visible';
-//}else{
-//	if (kaars.style.visibility === 'visible'){
-//		volgendknop.style.visibility=== 'visible';
-//	}else{volgendknop.style.visibility==='hidden'}
- 
+
 console.log ("naar volgend "+level);
 actueellevel(level);
 }
@@ -57,9 +50,7 @@ function naarvorig()
 if (level==13) {
 	level=7;
 
-	if (level==12) {
-		level=6;
-	}
+
 }
 
 console.log ("van  "+level);
@@ -109,7 +100,7 @@ console.log("actueellevel " + level);
 	document.getElementById("volgend").style.visibility="hidden" ;
 	document.getElementById("vorig").style.visibility="hidden";
 	} else { 
-		if (level==0 || level==1) {document.getElementById("vorig").style.visibility="hidden";} else 	{document.getElementById("vorig").style.visibility="visible";}
+		if (level==0 || level==1 || level==12) {document.getElementById("vorig").style.visibility="hidden";} else {document.getElementById("vorig").style.visibility="visible";}
 		if (level==3 || level==6) {
 			document.getElementById("extra1").style.visibility="visible";
 			document.getElementById("extra2").style.visibility="visible";
@@ -118,6 +109,13 @@ console.log("actueellevel " + level);
 			document.getElementById("extra2").style.visibility="hidden";
 			}
 		}
+
+if (level==12 || level==14) {
+	document.getElementById("volgend").style.visibility="hidden";} else {document.getElementById("volgend").style.visibility="visible";
+}
+
+
+
 
 /*voorwerpen*/
 		if (level==1) {
@@ -132,9 +130,25 @@ console.log("actueellevel " + level);
 		document.getElementById("sleutel").style.visibility="hidden";	
 		document.getElementById("kaars").style.visibility="hidden";}
 
-		if (level==2) {document.getElementById("mega").style.visibility="visible";}else{document.getElementById("mega").style.visibility="hidden";}
+	if (level==2) {document.getElementById("mega").style.visibility="visible";}else{document.getElementById("mega").style.visibility="hidden";}
+	if (level==12) {document.getElementById("mag").style.visibility="visible";}else{document.getElementById("mag").style.visibility="hidden";}
+	if (level==4) {document.getElementById("pen").style.visibility="visible";}else{document.getElementById("pen").style.visibility="hidden";}
+	if (level==4) {document.getElementById("papier").style.visibility="visible";}else{document.getElementById("papier").style.visibility="hidden";}
+	if (level==10) {document.getElementById("taart").style.visibility="visible";}else{document.getElementById("taart").style.visibility="hidden";}
+	if (level==10) {document.getElementById("ham").style.visibility="visible";}else{document.getElementById("ham").style.visibility="hidden";}
+	if (level!==9){document.getElementById("vuur").style.visibility="hidden"}
+	if (level!==7){document.getElementById("ham1").style.visibility="hidden"}
+	if (level!==7){document.getElementById("taart1").style.visibility="hidden"}
+	if (level==1) {document.getElementById("raam1").style.visibility="visible";}else{document.getElementById("raam1").style.visibility="hidden";}
+	if (level==2) {document.getElementById("raam2").style.visibility="visible";}else{document.getElementById("raam2").style.visibility="hidden";}
+	if (level==12) {document.getElementById("lichtknop").style.visibility="visible";}else{document.getElementById("Lichtknop").style.visibility="hidden";}
+	if (level==14) {
+		var audio = new Audio('skelet.mp3');
+		audio.play();}
 
-		if (level==12) {document.getElementById("mag").style.visibility="visible";}else{document.getElementById("mag").style.visibility="hidden";}
+if (level==8) {
+		var audio = new Audio('trompet.mp3');
+		audio.play();}
 
 kamer = [
 		"Het Kasteel",
@@ -151,7 +165,7 @@ kamer = [
 		"Boogkamer",
 		"Slaapkamer",
 		"Kerker",
-		"Oeps gaat niet verder" ];
+		"AAAAAH" ];
 
 document.getElementById("levelnummer").innerHTML = "Jouw locatie: "  + kamer[level];
 
@@ -164,14 +178,14 @@ console.log (document.getElementById("kasteel").src) ;
 
 /*verder*/
 function meldn(){
-var tekstmelden = ["Klik om te starten","Deur 2","Deur 3","Deur 4","Deur 5","Deur 6", "Deur 7", "Deur 8","Deur 9","Deur 10","Deur 6", "Deur dicht", "Deur val", "Deur gevangenis", "dood" ];
+var tekstmelden = ["Start","Deur 2","Deur 3","Rechter deur","Deur 5","Deur 6", "Rechter deur", "Deur 8","Deur 9","Deur 10","Deur 6", "Deur dicht", "Deur val", "Deur gevangenis"];
 var  loc=level
 return tekstmelden[loc];
 }
 
 /*terug*/
 function meldp(){
-var tekstmelden = ["Oeps hier kan je niet terug","Naar begin niet mogelijk","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Einde","Naar 3","Naar 9","Naar 6","Naar 3","Naar 6","Level 14" ];
+var tekstmelden = ["Oeps hier kan je niet terug","Naar begin niet mogelijk","Naar 1","Naar 2","Naar 3","Naar 4","Naar 5","Naar 6","Einde","Naar 3","Naar 9","Naar 6","Naar 3","Naar 6","snel terug"];
 var  loc=level
 return tekstmelden[loc];
 }
@@ -184,83 +198,84 @@ document.getElementById("invuil").style.visibility="visible";
 inhrugz[level]=true;
 }
 
-function uilmov(){
-document.getElementById("uiltje").style.visibility="visible";
-}
-
-function uildbl(){
-document.getElementById("uiltje").style.visibility="visible";
-document.getElementById("invuil").style.visibility="hidden";
-}
-
-
-
 function sleutelinv(){
 document.getElementById("sleutel").style.visibility="hidden";
 document.getElementById("invsleutel").style.visibility="visible";
-inhrugz[1]=true;
+inhrugz[level]=true;
 }
-
-function sleutelmov(){
-document.getElementById("sleutel").style.visibility="visible";
-}
-
-function sleuteldbl(){
-document.getElementById("sleutel").style.visibility="visible";
-document.getElementById("invsleutel").style.visibility="hidden";
-}
-
-
 
 function kaarsinv(){
 document.getElementById("kaars").style.visibility="hidden";
 document.getElementById("invkaars").style.visibility="visible";
-inhrugz[9]=true;
+inhrugz[level]=true;
 }
-
-
-function kaarsmov(){
-document.getElementById("kaars").style.visibility="visible";
-}
-
-function kaarsdbl(){
-document.getElementById("kaars").style.visibility="visible";
-document.getElementById("invkaars").style.visibility="hidden";
-}
-
-
 
 function megainv(){
 document.getElementById("mega").style.visibility="hidden";
 document.getElementById("invmega").style.visibility="visible";
-inhrugz[3]=true;
+inhrugz[level]=true;
 }
-
-
-function megamov(){
-document.getElementById("mega").style.visibility="visible";
-}
-
-function megadbl(){
-document.getElementById("mega").style.visibility="visible";
-document.getElementById("invmega").style.visibility="hidden";
-}
-
-
 
 function maginv(){
 document.getElementById("mag").style.visibility="hidden";
 document.getElementById("invmag").style.visibility="visible";
-inhrugz[12]=true;
+inhrugz[level]=true;
+}
+
+function taartinv(){
+document.getElementById("taart").style.visibility="hidden";
+document.getElementById("invtaart").style.visibility="visible";
+inhrugz[level]=true;
 }
 
 
-function magmov(){
-document.getElementById("mag").style.visibility="visible";
+function haminv(){
+document.getElementById("ham").style.visibility="hidden";
+document.getElementById("invham").style.visibility="visible";
+inhrugz[level]=true;
 }
 
-function magdbl(){
-document.getElementById("mag").style.visibility="visible";
-document.getElementById("invmag").style.visibility="hidden";
+function peninv(){
+document.getElementById("pen").style.visibility="hidden";
+document.getElementById("invpen").style.visibility="visible";
+inhrugz[level]=true;
 }
 
+function papierinv(){
+document.getElementById("papier").style.visibility="hidden";
+document.getElementById("invpapier").style.visibility="visible";
+inhrugz[level]=true; 
+}
+
+
+function vuuraan(){
+	if (level==9){
+document.getElementById("vuur").style.visibility="visible";}else{document.getElementById("vuur").style.visibility="hidden"}}
+
+function hamfun(){
+	if (level==7){
+document.getElementById("ham1").style.visibility="visible";}else{document.getElementById("ham1").style.visibility="hidden"}}
+
+function taartfun(){
+	if (level==7){
+document.getElementById("taart1").style.visibility="visible";}else{document.getElementById("taart1").style.visibility="hidden"}}
+
+function raamfun1(){
+	alert("Nee dit raam gaat echt niet open. En al kon het wel open, dan nog is dit geen goede vluchtweg. Tenzij je kunt vliegen natuurlijk.");
+}
+
+function raamfun2(){
+	alert("Nee, dit is echt niet de snelste manier om weg te komen. Ook dit raam gaat niet open. En als je niet kunt vliegen, is het openbreken van dit raam ook geen goed idee.");
+}
+
+function megatekst(){
+	if (level==13) {
+		alert("Ha ha, er is hier helemaal niemand. Je zult echt op eigen kracht de uitgang moeten zien te vinden! Jammer joh!")
+	}
+}
+
+function magtekst(){
+	if (level==7) {
+		alert("Tip! Neem de trap!")
+	}
+}	
